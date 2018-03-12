9f9aa7bab8789cfd19e6b28106c616df2cb930bd
Cobber <codecobber@gmail.com>
2018-03-12 16:34:09 +0000

    Add files via upload

---
+++ renamer.php
@@ -0,0 +1,25 @@
+<?php
+$dir='2017/april/';
+
+// Sort in ascending order - this is default
+$a = scandir($dir);
+
+// Sort in descending order
+$b = scandir($dir,1);
+
+//print_r($a);
+//print_r($b);
+
+//rename("2017/april/bunny.txt", "2017/april/bunny2.txt");
+
+foreach ($a as $name_of_file) {
+	if ($name_of_file == "." || $name_of_file == ".."){
+		continue;
+	}
+	
+	$oldname = $name_of_file;
+	echo $oldname . "<br>";
+	$tempName = str_replace(" ","_",$name_of_file);
+	rename($dir.$name_of_file, $dir.$tempName);
+}
+?>
